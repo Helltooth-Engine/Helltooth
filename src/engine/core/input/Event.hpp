5444391031c65201a700214a5b379dbe1d2aa677
raresica1234 <colorlord98@gmail.com>
2017-12-17 15:07:44 +0200

    Changed event handling

--- src/engine/core/input/Event.hpp
+++ src/engine/core/input/Event.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <iostream>
+
 #include "Keys.hpp"
+
+#include "utils/Log.hpp"
 
 namespace ht { namespace core {
 
@@ -13,30 +17,39 @@ namespace ht { namespace core {
 		ALT = 0x100,
 		ALTGR = 0x200
 	};
+	
+	enum class EventType {
+		UNKNOWN = -1,
+		MOUSE = 1,
+		KEYBOARD = 2
+	};
 
 	struct Event {
-		float m_MouseX, m_MouseY;
-		bool m_MouseButtons[HT_MAX_MOUSE_BUTTONS];
-		char m_KeyboardKeys[HT_MAX_KEYBOARD_KEYS];
+		EventType m_EventType;
+		union {
+			struct {
+				float x, y;
+				char mouseButton;
+				char mouseButtonState;
+			} m_Mouse;
+
+			struct {
+				char keyboard;
+				char state;
+				int modifiers;
+			} m_Key;
+		};
 
 		Event() {
-			m_MouseX = -1.f;
-			m_MouseY = -1.f;
-			memset(m_MouseButtons, 0, HT_MAX_MOUSE_BUTTONS);
-			memset(m_KeyboardKeys, 0, HT_MAX_KEYBOARD_KEYS);
+			m_EventType = EventType::UNKNOWN;
+			m_Mouse.x = -1.f;
+			m_Mouse.y = -1.f;
+			m_Mouse.mouseButton = -1;
+			m_Mouse.mouseButtonState - 1;
 		}
 
 		bool HasModifier(Modifier modifier) const {
-			switch (modifier) {
-			case Modifier::SHIFT:
-				return m_KeyboardKeys[HT_KEY_LEFT_SHIFT] || m_KeyboardKeys[HT_KEY_RIGHT_SHIFT];
-			case Modifier::CTRL:
-				return m_KeyboardKeys[HT_KEY_LEFT_CONTROL] || m_KeyboardKeys[HT_KEY_RIGHT_CONTROL];
-			case Modifier::ALT:
-				return m_KeyboardKeys[HT_KEY_LEFT_ALT];
-			case Modifier::ALTGR:
-				return m_KeyboardKeys[HT_KEY_RIGHT_ALT];
-			}
+			HT_ASSERT(m_EventType != EventType::KEYBOARD, "[Event] Can't check modifier if event is not keyboard type.");
 		}
 
 	};
