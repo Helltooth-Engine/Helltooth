e8ac7baa859ff5edd2f5c9d2dd709786dc34e3b4
raresica1234 <colorlord98@gmail.com>
2017-12-19 21:36:39 +0200

    Fixes

--- src/engine/Helltooth.hpp
+++ src/engine/Helltooth.hpp
@@ -4,5 +4,19 @@
 #endif
 
 #include "core/Window.hpp"
+#include "core/input/Event.hpp"
+#include "core/input/EventDispatcher.hpp"
+#include "core/input/EventListener.hpp"
+#include "core/input/Keys.hpp"
+
+#include "graphics/Context.hpp"
+#include "graphics/buffers/VertexBuffer.hpp"
+
+#include "maths/Mathcommon.hpp"
+#include "maths/Vector2.hpp"
+#include "maths/Vector3.hpp"
+#include "maths/Vector4.hpp"
+#include "maths/Matrix4.hpp"
 
 #include "utils/Log.hpp"
+#include "utils/String.hpp"
