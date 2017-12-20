1fbd8ead281f96d209918067c3f7f86676d206b2
raresica1234 <colorlord98@gmail.com>
2017-12-20 21:16:01 +0200

    Data Types

---
+++ src/engine/core/Internal.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace ht { namespace core {
+
+	typedef unsigned char byte;
+
+	typedef signed char s8;
+	typedef unsigned char u8;
+
+	typedef signed short s16;
+	typedef unsigned short u16;
+
+	typedef signed int s32;
+	typedef unsigned int u32;
+
+	typedef signed long long s64;
+	typedef unsigned long long u64;
+
+	typedef float f32;
+	typedef double f64;
+
+} }
