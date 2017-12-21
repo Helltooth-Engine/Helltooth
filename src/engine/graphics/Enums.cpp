94e60d8654fa1144898aaed4b2fabd7b059af3c2
raresica1234 <colorlord98@gmail.com>
2017-12-21 19:55:45 +0200

    Added more Data Types

--- src/engine/graphics/Enums.cpp
+++ src/engine/graphics/Enums.cpp
@@ -8,6 +8,10 @@ namespace ht { namespace graphics {
 		case DataType::UNSIGNED_SHORT: return sizeof(u16);
 		case DataType::UNSIGNED_INT: return sizeof(u32);
 		case DataType::UNSIGNED_BYTE: return sizeof(u8);
+		case DataType::VECTOR2: return sizeof(f32) * 2;
+		case DataType::VECTOR3: return sizeof(f32) * 3;
+		case DataType::VECTOR4: return sizeof(f32) * 4;
+		case DataType::MATRIX4: return sizeof(f32) * 16;
 		}
 		HT_ASSERT("[Enums] DataType not recognized!");
 	}
