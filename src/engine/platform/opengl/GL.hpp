07a28cb70f40e0172381b192716618509870c0d8
raresica1234 <colorlord98@gmail.com>
2017-12-25 12:05:04 +0200

    Added Core profile definition for wgl

--- src/engine/platform/opengl/GL.hpp
+++ src/engine/platform/opengl/GL.hpp
@@ -101,19 +101,21 @@ HT_CREATE_FUNCTION_POINTER(void, glDeleteProgram, GLuint program);
 
 extern bool GLInit();
 
-#define WGL_DRAW_TO_WINDOW_ARB            0x2001
-#define WGL_SUPPORT_OPENGL_ARB            0x2010
-#define WGL_DOUBLE_BUFFER_ARB             0x2011
-#define WGL_PIXEL_TYPE_ARB                0x2013
-#define WGL_TYPE_RGBA_ARB                 0x202B
-#define WGL_COLOR_BITS_ARB                0x2014
-#define WGL_DEPTH_BITS_ARB                0x2022
-#define WGL_STENCIL_BITS_ARB              0x2023
-#define WGL_CONTEXT_MAJOR_VERSION_ARB     0x2091
-#define WGL_CONTEXT_MINOR_VERSION_ARB     0x2092
-#define WGL_CONTEXT_LAYER_PLANE_ARB       0x2093
-#define WGL_CONTEXT_FLAGS_ARB             0x2094
-#define WGL_CONTEXT_DEBUG_BIT_ARB         0x00000001
+#define WGL_DRAW_TO_WINDOW_ARB						0x2001
+#define WGL_SUPPORT_OPENGL_ARB						0x2010
+#define WGL_DOUBLE_BUFFER_ARB						0x2011
+#define WGL_PIXEL_TYPE_ARB							0x2013
+#define WGL_TYPE_RGBA_ARB							0x202B
+#define WGL_COLOR_BITS_ARB							0x2014
+#define WGL_DEPTH_BITS_ARB							0x2022
+#define WGL_STENCIL_BITS_ARB						0x2023
+#define WGL_CONTEXT_MAJOR_VERSION_ARB				0x2091
+#define WGL_CONTEXT_MINOR_VERSION_ARB				0x2092
+#define WGL_CONTEXT_LAYER_PLANE_ARB					0x2093
+#define WGL_CONTEXT_FLAGS_ARB						0x2094
+#define WGL_CONTEXT_DEBUG_BIT_ARB					0x00000001
+#define WGL_CONTEXT_CORE_PROFILE_BIT_ARB			0x00000001
+#define WGL_CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB	0x00000002
 
 typedef BOOL(WINAPI * PFNWGLCHOOSEPIXELFORMATARBPROC) (HDC hdc, const int *piAttribIList, const FLOAT *pfAttribFList, UINT nMaxFormats, int *piFormats, UINT *nNumFormats);
 typedef HGLRC(WINAPI * PFNWGLCREATECONTEXTATTRIBSARBPROC) (HDC hDC, HGLRC hShareContext, const int *attribList);
