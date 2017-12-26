b6e08a3c34e77889786cb45d9d0e2232675f32d5
raresica1234 <colorlord98@gmail.com>
2017-12-26 17:44:26 +0200

    Vertex Array testing code

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -22,32 +22,38 @@ int main() {
 		0.0f, 1.f
 	};
 
-	u32 a;
-	glGenVertexArrays(1, &a);
-	glBindVertexArray(a);
 
 	BufferLayout layout = BufferLayout();
 	layout.AddLayout<float>(2, 2, false);
 
-	VertexBuffer* buffer = new VertexBuffer(positions, sizeof(positions), layout, BufferUsage::STATIC);
-
 	Shader* shader = new Shader("/res/glshader.vert", "/res/glshader.frag");
+
+	VertexBuffer* vbo = new VertexBuffer(positions, sizeof(positions), BufferUsage::STATIC);
+	vbo->Bind();
+	VertexArray* vao = new VertexArray(layout);
+
+	vao->Bind();
+
 #ifdef HT_OPENGL
 	glClearColor(0.3f, 0.4f, 0.7f, 1.0f);
 #endif
+
 	shader->Start();
 	while (!window.ShouldClose()) {
 		window.Clear();
+
 #ifdef HT_DIRECTX
 		Window::GetWindow()->GetDeviceContext()->Draw(6, 0);
 #else
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 #endif
+
 		window.Update();
 	}
 
 	delete shader;
-	delete buffer;
+	delete vbo;
+	delete vao;
 
 	system("PAUSE");
 	return 0;
