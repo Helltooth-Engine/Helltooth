02fa52307a2086f527dc136e37ad11527314272c
raresica1234 <colorlord98@gmail.com>
2017-12-26 13:15:08 +0200

    Removed player

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -15,7 +15,6 @@ int main() {
 	Window window(L"Window", 1280, 720);
 
 	VFS::Mount("res", "res/shaders/");
-	Shader* shader = new Shader("/res/glshader.vert", "/res/glshader.frag");
 
 	float positions[] = {
 		-1.f, -1.f,
@@ -27,31 +26,28 @@ int main() {
 	glGenVertexArrays(1, &a);
 	glBindVertexArray(a);
 
-
 	BufferLayout layout = BufferLayout();
 	layout.AddLayout<float>(2, 2, false);
 
 	VertexBuffer* buffer = new VertexBuffer(positions, sizeof(positions), layout, BufferUsage::STATIC);
 
-	Player player = Player();
-	EventDispatcher::Add((EventListener*)&player);
+	Shader* shader = new Shader("/res/glshader.vert", "/res/glshader.frag");
 #ifdef HT_OPENGL
 	glClearColor(0.3f, 0.4f, 0.7f, 1.0f);
 #endif
 	shader->Start();
-	buffer->Bind();
 	while (!window.ShouldClose()) {
 		window.Clear();
 #ifdef HT_DIRECTX
 		Window::GetWindow()->GetDeviceContext()->Draw(6, 0);
 #else
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 #endif
 		window.Update();
 	}
 
-	delete shader, buffer;
-	EventDispatcher::Remove(&player);
+	delete shader;
+	delete buffer;
 
 	system("PAUSE");
 	return 0;
