33a52778c580830b51cb13a9e938e70c6af58910
raresica1234 <colorlord98@gmail.com>
2017-12-31 12:25:10 +0200

    Updated demo code

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -18,29 +18,29 @@ int main() {
 
 	float positions[] = {
 		-.5f, -.5f,
-		.5f, -.5f,
-		0.0f, .5f
+		0.0f, .5f,
+		.5f, -.5f
 	};
 
 
-	BufferLayout layout = BufferLayout();
+	BufferLayout layout;
 	layout.AddLayout<float>("POSITION", 2, 2, false);
 
-	VertexBuffer* vbo = new VertexBuffer(positions, sizeof(positions), BufferUsage::STATIC);
 
 #ifdef HT_OPENGL
 	Shader* shader = new Shader(layout, "/res/glshader.vert", "/res/glshader.frag");
 	glClearColor(0.3f, 0.4f, 0.7f, 1.0f);
 #elif defined(HT_DIRECTX)
 	Shader* shader = new Shader(layout, "/res/dxshader.vert", "/res/dxshader.frag");
-
 #endif
-	shader->Start();
-	vbo->Bind(shader->GetStride());
+	VertexBuffer* vbo = new VertexBuffer(positions, sizeof(positions), BufferUsage::STATIC);
+	
+
 
 	while (!window.ShouldClose()) {
 		window.Clear();
-
+		vbo->Bind(shader->GetStride());
+		shader->Start();
 #ifdef HT_DIRECTX
 		DIRECTX_CONTEXT->Draw(3, 0);
 #else
--- src/demo/res/shaders/dxshader.frag
+++ src/demo/res/shaders/dxshader.frag
@@ -1,3 +1,3 @@
 float4 main(float4 position : SV_POSITION, float4 color : COLOR) : SV_Target0 {
-	return color;
+	return float4(color.x + 0.5, color.y + 0.5, color.z + 0.5, color.w);
 }
