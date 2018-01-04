31d807e8a06a301f52a0d4c369c6fa3a87fc96ff
raresica1234 <colorlord98@gmail.com>
2018-01-04 23:47:24 +0200

    Added Texture macros and wrapped more functions

--- src/engine/platform/opengl/GL.hpp
+++ src/engine/platform/opengl/GL.hpp
@@ -117,6 +117,45 @@ HT_CREATE_FUNCTION_POINTER(void, glGetProgramiv, GLuint program, GLenum pname, G
 HT_CREATE_FUNCTION_POINTER(void, glGetProgramInfoLog, GLuint program, GLsizei maxLength, GLsizei *length, GLchar *infoLog);
 HT_CREATE_FUNCTION_POINTER(GLuint, glGetUniformBlockIndex, GLuint program, const GLchar* uniformBlockName);
 HT_CREATE_FUNCTION_POINTER(void, glUniformBlockBinding, GLuint program, GLuint uniformBlockIndex, GLuint uniformBlockBinding);
+HT_CREATE_FUNCTION_POINTER(void, glUniform1iv, GLint location, GLsizei count, const GLint* value);
+HT_CREATE_FUNCTION_POINTER(GLint, glGetUniformLocation, GLuint program, const GLchar* name);
+
+//Texture
+#define GL_TEXTURE0 0x84C0
+#define GL_TEXTURE1 0x84C1
+#define GL_TEXTURE2 0x84C2
+#define GL_TEXTURE3 0x84C3
+#define GL_TEXTURE4 0x84C4
+#define GL_TEXTURE5 0x84C5
+#define GL_TEXTURE6 0x84C6
+#define GL_TEXTURE7 0x84C7
+#define GL_TEXTURE8 0x84C8
+#define GL_TEXTURE9 0x84C9
+#define GL_TEXTURE10 0x84CA
+#define GL_TEXTURE11 0x84CB
+#define GL_TEXTURE12 0x84CC
+#define GL_TEXTURE13 0x84CD
+#define GL_TEXTURE14 0x84CE
+#define GL_TEXTURE15 0x84CF
+#define GL_TEXTURE16 0x84D0
+#define GL_TEXTURE17 0x84D1
+#define GL_TEXTURE18 0x84D2
+#define GL_TEXTURE19 0x84D3
+#define GL_TEXTURE20 0x84D4
+#define GL_TEXTURE21 0x84D5
+#define GL_TEXTURE22 0x84D6
+#define GL_TEXTURE23 0x84D7
+#define GL_TEXTURE24 0x84D8
+#define GL_TEXTURE25 0x84D9
+#define GL_TEXTURE26 0x84DA
+#define GL_TEXTURE27 0x84DB
+#define GL_TEXTURE28 0x84DC
+#define GL_TEXTURE29 0x84DD
+#define GL_TEXTURE30 0x84DE
+#define GL_TEXTURE31 0x84DF
+
+HT_CREATE_FUNCTION_POINTER(void, glActiveTexture, GLenum texture);
+
 
 extern bool GLInit();
 
