694aa0a92d5dc36f38efda7e52230257aa426b5c
raresica1234 <colorlord98@gmail.com>
2018-01-07 23:40:09 +0200

    Forgot to push the actual texture loading

---
+++ src/engine/core/Asset.cpp
@@ -0,0 +1,39 @@
+#include "Asset.hpp"
+
+namespace ht { namespace core {
+	using namespace Cereal;
+
+	graphics::Texture* Asset::LoadTexture(utils::String path) {
+		Buffer buffer(1);
+		buffer.readFile(utils::VFS::Resolve(path).GetData());
+
+		Database* database = new Database();
+		database->read(buffer);
+
+		graphics::Texture* result = nullptr;
+
+		Object* object = database->getObject("texture2D");
+		if (object != nullptr) {
+			u32 width = (u32)object->getField("width")->getValue<s32>();
+			u32 height = (u32)object->getField("height")->getValue<s32>();
+			u32 bpp = (u32)object->getField("bpp")->getValue<s32>();
+			
+			Array* pArray = object->getArray("pixels");
+
+			byte* pixels = pArray->getRawArray<byte>(new byte[pArray->getCount()]);
+
+			result = new graphics::Texture2D(pixels, width, height, graphics::GetFormat(bpp));
+			
+			delete[] pixels;
+		}
+
+		delete database;
+
+		if (result == nullptr) {
+			HT_FATAL("[Asset] Texture format not supported!");
+		}
+
+		return result;
+	}
+
+} }
---
+++ src/engine/core/Asset.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Internal.hpp"
+
+#include "cereal/Cereal.hpp"
+
+
+#include "graphics/Texture.hpp"
+#include "graphics/Texture2D.hpp"
+
+#include "utils/String.hpp"
+#include "utils/Vfs.hpp"
+
+namespace ht { namespace core {
+
+	class Asset {
+	public:
+		static graphics::Texture* LoadTexture(utils::String path);
+
+
+	};
+	
+
+} }
