374106e58735f7d7249c11a4ff1224fac6690b8a
raresica1234 <colorlord98@gmail.com>
2018-01-15 20:57:18 +0200

    Texture base format

--- src/demo/res/shaders/glshader.frag
+++ src/demo/res/shaders/glshader.frag
@@ -7,5 +7,5 @@ uniform sampler2D textures[2];
 layout (location = 0) out vec4 color;
 
 void main() {
-	color = texture(textures[0], uvs).zyxw;
+	color = texture(textures[0], uvs);
 }
--- src/engine/platform/opengl/GLTexture2D.cpp
+++ src/engine/platform/opengl/GLTexture2D.cpp
@@ -4,38 +4,38 @@
 namespace ht { namespace graphics {
 
 	void Texture2D::SetSamplerState() {
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		GL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
+		GL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST));
+		GL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT));
+		GL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT));
 	}
 
 	Texture2D::Texture2D(u32 width, u32 height, TextureFormat format) : Texture(width, height, TextureType::TEXTURE_2D, format) {
-		glGenTextures(1, &m_Texture);
-		glBindTexture(GL_TEXTURE_2D, m_Texture);
+		GL(glGenTextures(1, &m_Texture));
+		GL(glBindTexture(GL_TEXTURE_2D, m_Texture));
 		SetSamplerState();
-		glTexImage2D(GL_TEXTURE_2D, 0, (GLenum)format, width, height, 0, (GLenum)format, GL_UNSIGNED_BYTE, nullptr);
+		GL(glTexImage2D(GL_TEXTURE_2D, 0, (GLenum)format, width, height, 0, (GLenum)GetBaseFormat(format), GL_UNSIGNED_BYTE, nullptr));
 	}
 
 	Texture2D::Texture2D(byte* pixels, u32 width, u32 height, TextureFormat format) : Texture(width, height, TextureType::TEXTURE_2D, format) {
-		glGenTextures(1, &m_Texture);
-		glBindTexture(GL_TEXTURE_2D, m_Texture);
+		GL(glGenTextures(1, &m_Texture));
+		GL(glBindTexture(GL_TEXTURE_2D, m_Texture));
 		SetSamplerState();
-		glTexImage2D(GL_TEXTURE_2D, 0, (GLenum)format, width, height, 0, (GLenum)format, GL_UNSIGNED_BYTE, pixels);
+		GL(glTexImage2D(GL_TEXTURE_2D, 0, (GLenum)format, width, height, 0, (GLenum)GetBaseFormat(format), GL_UNSIGNED_BYTE, pixels));
 	}
 
 	Texture2D::~Texture2D() {
-		glDeleteTextures(1, &m_Texture);
+		GL(glDeleteTextures(1, &m_Texture));
 	}
 
 	void Texture2D::SetPixels(byte* pixels) {
-		glBindTexture(GL_TEXTURE_2D, m_Texture);
-		glTexImage2D(GL_TEXTURE_2D, 0, (GLenum)m_Format, m_Width, m_Height, 0, (GLenum)m_Format, GL_UNSIGNED_BYTE, pixels);
+		GL(glBindTexture(GL_TEXTURE_2D, m_Texture));
+		GL(glTexImage2D(GL_TEXTURE_2D, 0, (GLenum)m_Format, m_Width, m_Height, 0, (GLenum)GetBaseFormat(m_Format), GL_UNSIGNED_BYTE, pixels));
 	}
 
 	void Texture2D::Bind(u32 slot) {
-		glBindTexture(GL_TEXTURE_2D, m_Texture);
-		glActiveTexture(GL_TEXTURE0 + slot);
+		GL(glBindTexture(GL_TEXTURE_2D, m_Texture));
+		GL(glActiveTexture(GL_TEXTURE0 + slot));
 	}
 
 
