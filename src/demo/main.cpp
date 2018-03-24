d24a8eab86c394e46fed24df5843663d52a9f91d
raresica1234 <colorlord98@gmail.com>
2018-03-24 23:33:20 +0200

    Demo now uses Application from the engine

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -10,93 +10,115 @@ using namespace maths;
 using namespace utils;
 using namespace graphics;
 
-int main() {
-	Window window(L"Window", 1280, 720);
+class Game : public Application {
+private:
+	Camera* camera;
+	BufferLayout* layout;
+	Entity* entity;
+	Shader* shader;
+	VertexBuffer* vbo;
+	IndexBuffer* ibo;
+	UniformBuffer* buffer;
+	Texture* texture;
+	Matrix4 proj, viewMatrix;
 
-	VFS::Mount("res", "res/shaders/");
-	VFS::Mount("res", "res/textures/");
 
-	float positions[] = {
-		-0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
-		-0.5f,  0.5f, -1.0f, 0.0f, 1.0f,
-		 0.5f,  0.5f, -1.0f, 1.0f, 1.0f,
-		 0.5f, -0.5f, -1.0f, 1.0f, 0.0f
-	};
+public:
+	// In here you should initialize stuff that's not depended to the rendering context
+	Game() 
+		: Application(L"Window", 1280, 720) {
+		VFS::Mount("res", "res/shaders/");
+		VFS::Mount("res", "res/textures/");
 
-	u16 indices[] = {
-		1, 2, 0,
-		2, 3, 0
-	};
+		camera = new FPSCamera(0, 0, 0);
+		entity = new Entity();
+	}
 
-	Camera* camera = new FPSCamera(0, 0, 0);
+	~Game() {
+		delete camera, layout, entity, shader, vbo, ibo, buffer, texture;
+	}
 
-	BufferLayout* layout = new BufferLayout();
-	layout->AddLayout<float>("UV", 2, 2, false);
-	layout->AddLayout<float>("POSITION", 3, 3, false);
+	void Init() {
+		float positions[] = {
+			-0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
+			-0.5f,  0.5f, -1.0f, 0.0f, 1.0f,
+			0.5f,  0.5f, -1.0f, 1.0f, 1.0f,
+			0.5f, -0.5f, -1.0f, 1.0f, 0.0f
+		};
 
-	Entity* entity = new Entity();
-	Shader* shader = new Shader(layout, "/res/shader.vert", "/res/shader.frag", ShaderLocationType::FROM_PATH | ShaderLocationType::FROM_HTSL);
+		u16 indices[] = {
+			1, 2, 0,
+			2, 3, 0
+		};
+
+		layout = new BufferLayout();
+		layout->AddLayout<float>("UV", 2, 2, false);
+		layout->AddLayout<float>("POSITION", 3, 3, false);
+
+
+		shader = new Shader(layout, "/res/shader.vert", "/res/shader.frag", ShaderLocationType::FROM_PATH | ShaderLocationType::FROM_HTSL);
+		vbo = new VertexBuffer(positions, sizeof(positions), BufferUsage::STATIC);
 
 #ifdef HT_OPENGL
-	s32 ids[] = { 0, 1 };
-	shader->SetSamplers(ids, 2);
+		s32 ids[] = { 0, 1 };
+		shader->SetSamplers(ids, 2);
 
-	glClearColor(0.3f, 0.4f, 0.7f, 1.0f);
+		glClearColor(0.3f, 0.4f, 0.7f, 1.0f);
 #endif
-	VertexBuffer* vbo = new VertexBuffer(positions, sizeof(positions), BufferUsage::STATIC);
-	
-	IndexBuffer* ibo = new IndexBuffer(indices, 6, BufferUsage::STATIC);
 
-	UniformBufferLayout ulayout(ShaderType::VERTEX);
-	ulayout.AddUniform<Matrix4>();
-	ulayout.AddUniform<Matrix4>();
-	ulayout.AddUniform<Matrix4>();
+		ibo = new IndexBuffer(indices, 6, BufferUsage::STATIC);
 
-	Matrix4 proj = Matrix4::CreatePerspective(70, 0.01f, 1000.0f, 1.77f);
-	Matrix4 viewMatrix = Matrix4(1.0f);
-	UniformBuffer* buffer = new UniformBuffer(ulayout);
-	buffer->Set(0, &proj[0]);
-	buffer->Set(1, &viewMatrix[0]);
-	buffer->Set(2, &entity->GetModelMatrix()[0]);
+		UniformBufferLayout ulayout(ShaderType::VERTEX);
+		ulayout.AddUniform<Matrix4>();
+		ulayout.AddUniform<Matrix4>();
+		ulayout.AddUniform<Matrix4>();
 
-	Texture* texture = Asset::LoadTexture("/res/final_logo.httexture");
+		proj = Matrix4::CreatePerspective(70, 0.01f, 1000.0f, 1.77f);
+		viewMatrix = Matrix4(1.0f);
 
-	texture->Bind(0);
-	int rot = 0;
-	Timer timer;
-
-	vbo->Bind(shader->GetStride()); 
-	shader->Start();
-	ibo->Bind();
-	buffer->Bind();
-	float delta = 0.0f;
-	while (!window.ShouldClose()) {
-		window.Clear();
-		camera->Update(delta);
-		entity->Rotate(50.f * delta, 50.f * delta, 50.f * delta);
-
-		entity->Update(delta);
-		
-		viewMatrix = camera->GetViewMatrix();
-
+		buffer = new UniformBuffer(ulayout);
+		buffer->Set(0, &proj[0]);
 		buffer->Set(1, &viewMatrix[0]);
 		buffer->Set(2, &entity->GetModelMatrix()[0]);
-		buffer->Bind();
 
+		texture = Asset::LoadTexture("/res/final_logo.httexture");
+
+		texture->Bind(0);
+
+		vbo->Bind(shader->GetStride());
+		shader->Start();
+		ibo->Bind();
+		buffer->Bind();
+	}
+
+	void Render() {
 #ifdef HT_DIRECTX
 		HT_DXCONTEXT->DrawIndexed(ibo->GetCount(), 0, 0);
 #else
 		glDrawElements(GL_TRIANGLES, ibo->GetCount(), ibo->GetFormat(), 0);
 #endif
-
-		window.Update();
-		delta = timer.GetDelta();
-		float frames = 1.0f / delta;
-		window.SetTitle(L"Window | frames: " + std::to_wstring(frames));
 	}
 
-	delete shader, vbo, buffer, camera, layout, entity, texture, ibo;
+	void Update(float delta) {
+		camera->Update(delta);
+		entity->Rotate(50.f * delta, 50.f * delta, 50.f * delta);
+		entity->Update(delta);
+		viewMatrix = camera->GetViewMatrix();
 
-	//system("PAUSE");
+		buffer->Set(1, &viewMatrix[0]);
+		buffer->Set(2, &entity->GetModelMatrix()[0]);
+		buffer->Bind();
+	}
+
+	void Tick() {
+
+	}
+
+};
+
+int main() {
+	Game game;
+	game.Start();
+
 	return 0;
 }
