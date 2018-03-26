33c40f136287c969ffacf59b8ad6ef6bf1559649
OEM Configuration (temporary user) <colorlord98@gmail.com>
2018-03-26 20:25:51 +0300

    Intrinsics for Linux

--- src/engine/maths/Mathcommon.hpp
+++ src/engine/maths/Mathcommon.hpp
@@ -1,12 +1,16 @@
 #pragma once
 
-#ifdef HT_WINDOWS
+#if defined(HT_WINDOWS)
 #include <intrin.h>
+#elif defined(HT_LINUX)
+#include <x86intrin.h>
 #else
 #error Other platforms not supported.
 #endif
 
+#if !defined(M_PI)
 #define M_PI 3.14159265358979323846264338327950288419716939937510f
+#endif
 
 #define ToRadians(x) x * 0.01745329251f
 #define ToDegrees(x) x * 57.2957795131f
