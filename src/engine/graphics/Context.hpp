93dab156fd1318637c5804823c861f5cfd57a0b6
OEM Configuration (temporary user) <colorlord98@gmail.com>
2018-03-26 22:04:37 +0300

    Fixed type in Context

--- src/engine/graphics/Context.hpp
+++ src/engine/graphics/Context.hpp
@@ -62,7 +62,7 @@ namespace ht { namespace graphics {
 #if defined(HT_LINUX)
 		inline Colormap& GetColormap() { return m_ColorMap; }
 		inline XSetWindowAttributes& GetWindowAttributes() { return m_WindowAttributes; }
-		inline XVisualINfo* GetVisualInfo() { return m_VisualInfo; }
+		inline XVisualInfo* GetVisualInfo() { return m_VisualInfo; }
 #endif
 
 	};
