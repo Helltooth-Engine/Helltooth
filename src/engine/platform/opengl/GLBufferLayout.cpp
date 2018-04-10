9372889a7e30c73911362fbdf90d0877a09cca26
raresica1234 <colorlord98@gmail.com>
2018-04-10 16:01:47 +0300

    GL implementation for data in multiple buffers

--- src/engine/platform/opengl/GLBufferLayout.cpp
+++ src/engine/platform/opengl/GLBufferLayout.cpp
@@ -3,7 +3,7 @@
 
 namespace ht { namespace graphics {
 
-	BufferLayout::BufferLayout() : m_Stride(0) {
+	BufferLayout::BufferLayout(){
 		GL(glGenVertexArrays(1, &m_VaoID));
 	}
 
@@ -15,12 +15,35 @@ namespace ht { namespace graphics {
 
 	void BufferLayout::Bind() {
 		glBindVertexArray(m_VaoID);
+	}
+
+	void BufferLayout::Start(VertexBuffer** buffers) {
+		u32 attribOffset = 0;
 		u32 offset = 0;
+		if (buffers != nullptr) {
+			buffers[0]->Bind();
+		}
+		int lastBuffer = 0;
 		for (u32 i = 0; i < m_Attributes.size(); i++) {
-			u8 dataSize = DataTypeSize(m_Attributes[i].type);
-			GL(glEnableVertexAttribArray(i));
-			GL(glVertexAttribPointer(i, m_Attributes[i].count, static_cast<GLenum>(m_Attributes[i].type), static_cast<GLboolean>(m_Attributes[i].normalized), m_Stride, reinterpret_cast<GLvoid*>(offset)));
-			offset += m_Attributes[i].count * dataSize;
+			if (m_Attributes[i + attribOffset].bufferId != lastBuffer) {
+				lastBuffer = m_Attributes[i + attribOffset].bufferId;
+				offset = 0;
+				buffers[lastBuffer]->Bind();
+			}
+			if (m_Attributes[i + attribOffset].type == DataType::MATRIX4) {
+				u8 dataSize = DataTypeSize(DataType::FLOAT);
+				for (int k = 0; k < 4; k++, attribOffset++) {
+					GL(glEnableVertexAttribArray(i + attribOffset));
+					GL(glVertexAttribPointer(i + attribOffset, m_Attributes[i].count / 4, static_cast<GLenum>(DataType::FLOAT), static_cast<GLboolean>(m_Attributes[i + attribOffset].normalized), m_Stride[lastBuffer], reinterpret_cast<GLvoid*>(offset)));
+					offset += m_Attributes[i + attribOffset].count * dataSize;
+				}
+			}
+			else {
+				u8 dataSize = DataTypeSize(m_Attributes[i].type);
+				GL(glEnableVertexAttribArray(i + attribOffset));
+				GL(glVertexAttribPointer(i + attribOffset, m_Attributes[i + attribOffset].count, static_cast<GLenum>(m_Attributes[i + attribOffset].type), static_cast<GLboolean>(m_Attributes[i + attribOffset].normalized), m_Stride[lastBuffer], reinterpret_cast<GLvoid*>(offset)));
+				offset += m_Attributes[i + attribOffset].count * dataSize;
+			}
 		}
 	}
 
