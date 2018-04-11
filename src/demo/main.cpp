3546cd59e448fdd9d46d8de0326dd816a11cbbf8
raresica1234 <colorlord98@gmail.com>
2018-04-11 20:52:11 +0300

    Updated demo with 500 cubes

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <Helltooth.hpp>
+#include <random>
+#include <time.h>
 
 using namespace ht;
 using namespace core;
@@ -11,8 +13,8 @@ using namespace entities;
 class Game : public Application {
 private:
 	Camera* camera;
-	Entity entity;
-	TransformComponent transform;
+	std::vector<Entity> entities;
+	std::vector<TransformComponent> transforms;
 	ModelComponent* model;
 	UniformBuffer* buffer;
 	Texture* texture;
@@ -23,23 +25,30 @@ public:
 	// In here you should initialize stuff that's not depended to the rendering context
 	Game() 
 		: Application("Window", 1280, 720) {
+		srand(time(NULL));
 		VFS::Mount("res", "res/shaders/");
 		VFS::Mount("res", "res/textures/");
 		VFS::Mount("res", "res/models/");
 
-		camera = new FPSCamera(0, 0, -5);
-		entity.AddComponent(&transform);
+		camera = new FPSCamera(0, 0, -100);
 	}
 
 	~Game() {
-		delete camera, renderer, entity, buffer, model, texture;
+		delete camera, renderer, buffer, model, texture;
 	}
 
 	void Init() {
 		proj = Matrix4::CreatePerspective(70, 0.01f, 1000.0f, 1.77f);
 		renderer = new Renderer(camera, proj);
 		model = Asset::LoadModel("/res/cube.htmodel");
-		entity.AddComponent(model);
+		transforms.resize(500);
+		for (int i = 0; i < 500; i++) {
+			entities.emplace_back();
+			entities[i].AddComponent(model);
+			transforms[i] = TransformComponent(Vector3((f32)rand() / (f32)RAND_MAX * 200.f, (f32)rand() / (f32)RAND_MAX * 200.f, (f32)rand() / (f32)RAND_MAX * 200.f));
+			//transforms[i].Scale(1, 1, 1);
+			entities[i].AddComponent(&transforms[i]);
+		}
 
 #ifdef HT_OPENGL
 		glClearColor(0.3f, 0.4f, 0.7f, 1.0f);
@@ -51,15 +60,18 @@ public:
 	}
 
 	void Render() {
-		renderer->Submit(entity);
+		for(u32 i=0; i < entities.size(); i++)
+			renderer->Submit(entities[i]);
 		renderer->Render();
 		renderer->End();
 	}
 
 	void Update(float delta) {
 		camera->Update(delta);
-		transform.Rotate(50.f * delta, 50.f * delta, 50.f * delta);
-		transform.Update(delta);
+		for (TransformComponent& transform : transforms) {
+			transform.Rotate(0, (rand() % 101) * delta, 0);
+			transform.Update(delta);
+		}
 	}
 
 	void Tick() {
