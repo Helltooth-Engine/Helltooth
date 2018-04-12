1eb58cfa0a62fce56f9e26dc188b4455dd243574
raresica1234 <colorlord98@gmail.com>
2018-04-12 12:39:49 +0300

    Added getter for ID3D11Buffer

--- src/engine/graphics/buffers/VertexBuffer.hpp
+++ src/engine/graphics/buffers/VertexBuffer.hpp
@@ -18,7 +18,7 @@ namespace ht { namespace graphics {
 
 	class VertexBuffer {
 	private:
-#ifdef HT_OPENGL
+#if defined(HT_OPENGL)
 		u32 m_Buffer;
 #elif defined(HT_DIRECTX)
 		ID3D11Buffer* m_Buffer;
@@ -33,6 +33,10 @@ namespace ht { namespace graphics {
 		void Unmap();
 
 		void Bind(u32 stride = 0) const;
+
+#if defined(HT_DIRECTX)
+		ID3D11Buffer* GetBuffer() const { return m_Buffer; }
+#endif
 	};
 
 } }
