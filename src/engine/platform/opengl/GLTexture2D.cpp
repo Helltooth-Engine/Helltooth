6a92c9e0acb7842d6f594b6b86e6f4db3d3ebf87
raresica1234 <colorlord98@gmail.com>
2018-04-12 12:54:16 +0300

    Added Texture mipmapping for OpenGL

--- src/engine/platform/opengl/GLTexture2D.cpp
+++ src/engine/platform/opengl/GLTexture2D.cpp
@@ -4,24 +4,26 @@
 namespace ht { namespace graphics {
 
 	void Texture2D::SetSamplerState() {
+		GL(glGenerateMipmap(GL_TEXTURE_2D));
 		GL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
-		GL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST));
+		GL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR));
 		GL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT));
 		GL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT));
+		GL(glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_LOD_BIAS, 0.4f));
 	}
 
 	Texture2D::Texture2D(u32 width, u32 height, TextureFormat format) : Texture(width, height, TextureType::TEXTURE_2D, format) {
 		GL(glGenTextures(1, &m_Texture));
 		GL(glBindTexture(GL_TEXTURE_2D, m_Texture));
-		SetSamplerState();
 		GL(glTexImage2D(GL_TEXTURE_2D, 0, static_cast<GLenum>(format), width, height, 0, static_cast<GLenum>(GetBaseFormat(format)), GL_UNSIGNED_BYTE, nullptr));
+		SetSamplerState();
 	}
 
 	Texture2D::Texture2D(byte* pixels, u32 width, u32 height, TextureFormat format) : Texture(width, height, TextureType::TEXTURE_2D, format) {
 		GL(glGenTextures(1, &m_Texture));
 		GL(glBindTexture(GL_TEXTURE_2D, m_Texture));
-		SetSamplerState();
 		GL(glTexImage2D(GL_TEXTURE_2D, 0, static_cast<GLenum>(format), width, height, 0, static_cast<GLenum>(GetBaseFormat(format)), GL_UNSIGNED_BYTE, pixels));
+		SetSamplerState();
 	}
 
 	Texture2D::~Texture2D() {
