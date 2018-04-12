6ac2bc4d4a834d7a34e687665fbbb95d2a57cc57
raresica1234 <colorlord98@gmail.com>
2018-04-12 17:24:11 +0300

    Added mipmapping to DirectX

--- src/engine/platform/directx/DXTexture2D.cpp
+++ src/engine/platform/directx/DXTexture2D.cpp
@@ -13,6 +13,7 @@ namespace ht { namespace graphics {
 		samplerDesc.MaxAnisotropy   = 1;
 		samplerDesc.ComparisonFunc  = D3D11_COMPARISON_ALWAYS;
 		samplerDesc.MaxLOD          = D3D11_FLOAT32_MAX;
+		samplerDesc.MipLODBias      = 0.4f;
 
 		HT_DXDEVICE->CreateSamplerState(&samplerDesc, &m_SamplerState);
 	}
@@ -24,12 +25,13 @@ namespace ht { namespace graphics {
 
 		textureDesc.Width               = width;
 		textureDesc.Width               = height;
-		textureDesc.MipLevels           = 1;
+		textureDesc.MipLevels           = 0;
 		textureDesc.ArraySize           = 1;
 		textureDesc.Format              = static_cast<DXGI_FORMAT>(GetBaseFormat(format));
-		textureDesc.BindFlags           = D3D11_BIND_SHADER_RESOURCE;
-		textureDesc.CPUAccessFlags      = D3D11_CPU_ACCESS_WRITE;
+		textureDesc.BindFlags           = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET;
+		textureDesc.CPUAccessFlags      = 0;
 		textureDesc.Usage               = D3D11_USAGE_DYNAMIC;
+		textureDesc.MiscFlags           = D3D11_RESOURCE_MISC_GENERATE_MIPS;
 		textureDesc.SampleDesc.Count    = 1;
 		textureDesc.SampleDesc.Quality  = 0;
 
@@ -39,10 +41,11 @@ namespace ht { namespace graphics {
 
 		viewDesc.Format                     = textureDesc.Format;
 		viewDesc.ViewDimension              = D3D11_SRV_DIMENSION_TEXTURE2D;
-		viewDesc.Texture2D.MipLevels        = 1;
+		viewDesc.Texture2D.MipLevels        = -1;
 		viewDesc.Texture2D.MostDetailedMip  = 0;
 
 		DX(HT_DXDEVICE->CreateShaderResourceView(m_Texture, &viewDesc, &m_ResourceView));
+		HT_DXCONTEXT->GenerateMips(m_ResourceView);
 	}
 
 	Texture2D::Texture2D(byte* pixels, u32 width, u32 height, TextureFormat format) : Texture(width, height, TextureType::TEXTURE_2D, format) {
@@ -52,30 +55,28 @@ namespace ht { namespace graphics {
 
 		textureDesc.Width               = width;
 		textureDesc.Height              = height;
-		textureDesc.MipLevels           = 1;
+		textureDesc.MipLevels           = 0;
 		textureDesc.ArraySize           = 1;
 		textureDesc.Format              = static_cast<DXGI_FORMAT>(GetBaseFormat(format));
-		textureDesc.BindFlags           = D3D11_BIND_SHADER_RESOURCE;
-		textureDesc.CPUAccessFlags      = D3D11_CPU_ACCESS_WRITE;
+		textureDesc.BindFlags           = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET;
+		textureDesc.CPUAccessFlags      = 0;
 		textureDesc.Usage               = D3D11_USAGE_DEFAULT;
+		textureDesc.MiscFlags           = D3D11_RESOURCE_MISC_GENERATE_MIPS;
 		textureDesc.SampleDesc.Count    = 1;
 		textureDesc.SampleDesc.Quality  = 0;
 
-		D3D11_SUBRESOURCE_DATA data = {};
-
-		data.pSysMem      = pixels;
-		data.SysMemPitch  = width * TextureFormatSize(format);
-
-		DX(HT_DXDEVICE->CreateTexture2D(&textureDesc, &data, &m_Texture));
+		DX(HT_DXDEVICE->CreateTexture2D(&textureDesc, NULL, &m_Texture));
+		HT_DXCONTEXT->UpdateSubresource(m_Texture, 0, NULL, pixels, width * TextureFormatSize(format), 0);
 
 		D3D11_SHADER_RESOURCE_VIEW_DESC viewDesc = {};
 
 		viewDesc.Format                     = textureDesc.Format;
 		viewDesc.ViewDimension              = D3D11_SRV_DIMENSION_TEXTURE2D;
-		viewDesc.Texture2D.MipLevels        = 1;
+		viewDesc.Texture2D.MipLevels        = -1;
 		viewDesc.Texture2D.MostDetailedMip  = 0;
 
 		DX(HT_DXDEVICE->CreateShaderResourceView(m_Texture, &viewDesc, &m_ResourceView));
+		HT_DXCONTEXT->GenerateMips(m_ResourceView);
 	}
 
 	Texture2D::~Texture2D() {
