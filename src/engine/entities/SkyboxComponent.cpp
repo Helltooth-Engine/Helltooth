52c8e9f53583d5b6845290f117ae56d79adaa297
raresica1234 <colorlord98@gmail.com>
2019-08-21 13:01:41 +0300

    Fixed assert being the other way around

--- src/engine/entities/SkyboxComponent.cpp
+++ src/engine/entities/SkyboxComponent.cpp
@@ -4,7 +4,7 @@ namespace ht { namespace entities {
 
 	SkyboxComponent::SkyboxComponent(graphics::Texture* texture) {
 		HT_ASSERT(texture != nullptr, "[SkyboxComponent] Texture cannot be null.");
-		HT_ASSERT(texture->GetType() != graphics::TextureType::TEXTURE_3D, "[SkyboxComponent] Texture needs to be 3D.");
+		HT_ASSERT(texture->GetType() == graphics::TextureType::TEXTURE_3D, "[SkyboxComponent] Texture needs to be 3D.");
 		m_TextureCube = static_cast<graphics::TextureCube*>(texture);
 	}
 
