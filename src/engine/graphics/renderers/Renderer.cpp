d49e8a816f553256182a6e97baeb90f704a38e7f
rares <colorlord98@gmail.com>
2019-10-26 15:04:31 +0300

    Forgot starting shader before setting samplers

--- src/engine/graphics/renderers/Renderer.cpp
+++ src/engine/graphics/renderers/Renderer.cpp
@@ -27,6 +27,8 @@ namespace ht { namespace graphics {
 				 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 
 				16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 
 			};
+			
+			m_Shader->Start();
 			m_Shader->SetSamplers(ids, 32);
 #endif // HT_OPENGL
 			m_OwnShader = true;
