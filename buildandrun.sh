924a9fe7755d3c63c8b32dbdc10de854bf6ddeee
rares <colorlord98@gmail.com>
2019-10-26 13:11:29 +0300

    Modified build and run script

--- buildandrun.sh
+++ buildandrun.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-res_location=res
+res_location=bin/Debug-GL/10000Cubes/res/
 
 if [ ! -d $res_location ]; then
 	cp -R src/demo/res $res_location
 fi
 
 make -d config=debug-gl_x64 -C Solution/
-./bin/Sandbox/Sandbox
+./bin/Debug-GL/10000Cubes/10000Cubes
