c6d767191ef36a12e361be0424d6f1384d59f649
rares <colorlord98@gmail.com>
2019-10-26 19:00:01 +0300

    Prettified code

--- src/engine/graphics/renderers/Renderer.hpp
+++ src/engine/graphics/renderers/Renderer.hpp
@@ -24,21 +24,21 @@ namespace ht { namespace graphics {
 
 	class Renderer {
 	private:
-		std::vector<Renderable>  m_Renderables;
+		std::vector<Renderable>				m_Renderables;
 
-		const entities::SkyboxComponent* m_SkyboxComponent = nullptr;
-		entities::ModelComponent* m_Quad = nullptr;
+		const entities::SkyboxComponent* 	m_SkyboxComponent = nullptr;
+		entities::ModelComponent* 			m_Quad = nullptr;
 
-		maths::Matrix4           m_Projection;
-		Shader*                  m_Shader;
-		Camera*                  m_Camera              = nullptr;
-		BufferLayout*            m_Layout              = nullptr;
-		UniformBuffer*           m_Uniform             = nullptr;
-		VertexBuffer*            m_InstanceData        = nullptr;
+		maths::Matrix4           			m_Projection;
+		Shader*                  			m_Shader;
+		Camera*                  			m_Camera              = nullptr;
+		BufferLayout*            			m_Layout              = nullptr;
+		UniformBuffer*           			m_Uniform             = nullptr;
+		VertexBuffer*            			m_InstanceData        = nullptr;
 
-		u32                      m_InstaceDataSize     = 0;
-		u32                      m_InstaceDataSizeLast = 0;
-		bool                     m_OwnShader           = false;
+		u32                      			m_InstaceDataSize     = 0;
+		u32                      			m_InstaceDataSizeLast = 0;
+		bool                     			m_OwnShader           = false;
 
 	public:
 		Renderer(Camera* camera, maths::Matrix4 projection, Shader* shader = nullptr);
