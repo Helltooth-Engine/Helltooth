cc372b6e0eaf0c38692eabecdd2c35220a1049f7
rares <colorlord98@gmail.com>
2020-03-28 13:02:34 +0200

    Modified generate script to have default premake argument for Linux

--- src/generate.sh
+++ src/generate.sh
@@ -1,12 +1,22 @@
 args=vs2019
 directory=../Solution
-
+uname=`uname`
 if [ $# -gt 0 ]; then
 	args=$@
+elif [ $uname = "Linux" ]; then
+	args=gmake2
+fi
+
+if [ -n "`ls -A $directory`" ]; then
+	rm -r $directory/
 fi
 
 if [ ! -d "$directory" ]; then
 	mkdir $directory
+fi
+
+if [ ! -d "$directory/Sandbox" ]; then
+	mkdir $directory/Sandbox
 fi
 
 cp -R demo/res ../Solution/Sandbox/
