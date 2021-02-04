e7880673115e9a3cc7bcbe41c9164178ae4dc925
rares <colorlord98@gmail.com>
2021-02-04 18:33:03 +0200

    generate.sh now takes into consideration the platform and properly choses the correct premake executable

--- src/generate.sh
+++ src/generate.sh
@@ -1,10 +1,12 @@
 args=vs2019
 directory=../Solution
 uname=`uname`
+premake=../bin/premake5.exe
 if [ $# -gt 0 ]; then
 	args=$@
 elif [ $uname = "Linux" ]; then
 	args=gmake2
+	premake=../bin/premake5
 fi
 
 if [ -n "`ls -A $directory`" ]; then
@@ -21,4 +23,4 @@ fi
 
 cp -R demo/res ../Solution/Sandbox/
 
-../bin/premake5 $args --file=premake/premake5.lua
+$premake $args --file=premake/premake5.lua
