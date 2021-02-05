4e856b3faf59a00ceb8f6803017b1dd744adc7a6
rares <colorlord98@gmail.com>
2021-02-05 14:26:31 +0200

    Changed build script to copy resources after the build has finished

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+echo "Building..."
+make -d config=debug-gl_x64 -C Solution/
 
+echo "Copying resources..."
 res_location=bin/Debug-GL/10000Cubes/res/
 
 if [ ! -d $res_location ]; then
 	cp -R src/demo/res $res_location
 fi
-
-make -d config=debug-gl_x64 -C Solution/
