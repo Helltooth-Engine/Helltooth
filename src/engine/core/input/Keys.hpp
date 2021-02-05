2edc44325ddf389337fa0dfd67c556a070f6e57e
rares <colorlord98@gmail.com>
2021-02-05 18:17:23 +0200

    KMS

--- src/engine/core/input/Keys.hpp
+++ src/engine/core/input/Keys.hpp
@@ -23,6 +23,10 @@
 
 #pragma once
 
+#define HT_KEY_UNKNOWN         -1
+
+#if defined(HT_WINDOWS)
+
 #define HT_MOUSE_LBUTTON        1
 #define HT_MOUSE_RBUTTON        2
 #define HT_MOUSE_BUTTON3        3
@@ -32,10 +36,6 @@
 #define HT_MOUSE_BUTTON7        7
 #define HT_MOUSE_BUTTON8        8
 #define HT_MOUSE_BUTTON9        9
-
-#define HT_KEY_UNKNOWN         -1
-
-#ifdef HT_WINDOWS
 
 #define HT_KEY_SPACE            32
 #define HT_KEY_APOSTROPHE       39 /* ' */
@@ -171,140 +171,148 @@
 #define HT_KEY_MENU             348
 
 #elif defined(HT_LINUX)
-
+#include <X11/keysym.h>
 // Todo: Printscreen keycode
 
-#define HT_KEY_ESCAPE           1
+#define HT_MOUSE_LBUTTON        1
+#define HT_MOUSE_BUTTON3        2
+#define HT_MOUSE_RBUTTON        3
+#define HT_MOUSE_BUTTON4        4
+#define HT_MOUSE_BUTTON5        5
+#define HT_MOUSE_BUTTON6        6
+#define HT_MOUSE_BUTTON7        7
+#define HT_MOUSE_BUTTON8        8
+#define HT_MOUSE_BUTTON9        9
 
-#define HT_KEY_1                2
-#define HT_KEY_2                3
-#define HT_KEY_3                4
-#define HT_KEY_4                5
-#define HT_KEY_5                6
-#define HT_KEY_6                7
-#define HT_KEY_7                8
-#define HT_KEY_8                9
-#define HT_KEY_9                10
-#define HT_KEY_0                11
+#define HT_KEY_ESCAPE           XK_Escape
 
-#define HT_KEY_Q                16
-#define HT_KEY_W                17
-#define HT_KEY_E                18
-#define HT_KEY_R                19
-#define HT_KEY_T                20
-#define HT_KEY_Y                21
-#define HT_KEY_U                22
-#define HT_KEY_I                23
-#define HT_KEY_O                24
-#define HT_KEY_P                25
+#define HT_KEY_1                XK_1
+#define HT_KEY_2                XK_2
+#define HT_KEY_3                XK_3
+#define HT_KEY_4                XK_4
+#define HT_KEY_5                XK_5
+#define HT_KEY_6                XK_6
+#define HT_KEY_7                XK_7
+#define HT_KEY_8                XK_8
+#define HT_KEY_9                XK_9
+#define HT_KEY_0                XK_0
 
-#define HT_KEY_A                30
-#define HT_KEY_S                31
-#define HT_KEY_D                32
-#define HT_KEY_F                33
-#define HT_KEY_G                34
-#define HT_KEY_H                35
-#define HT_KEY_J                36
-#define HT_KEY_K                37
-#define HT_KEY_L                38
+#define HT_KEY_Q                XK_q
+#define HT_KEY_W                XK_w
+#define HT_KEY_E                XK_e
+#define HT_KEY_R                XK_r
+#define HT_KEY_T                XK_t
+#define HT_KEY_Y                XK_y
+#define HT_KEY_U                XK_u
+#define HT_KEY_I                XK_i
+#define HT_KEY_O                XK_o
+#define HT_KEY_P                XK_p
 
-#define HT_KEY_Z                44
-#define HT_KEY_X                45
-#define HT_KEY_C                46
-#define HT_KEY_V                47
-#define HT_KEY_B                48
-#define HT_KEY_N                49
-#define HT_KEY_M                50
+#define HT_KEY_A                XK_a
+#define HT_KEY_S                XK_s
+#define HT_KEY_D                XK_d
+#define HT_KEY_F                XK_f
+#define HT_KEY_G                XK_g
+#define HT_KEY_H                XK_h
+#define HT_KEY_J                XK_j
+#define HT_KEY_K                XK_k
+#define HT_KEY_L                XK_l
 
-#define HT_KEY_TAB              15
-#define HT_KEY_MINUS            12
-#define HT_KEY_EQUAL            13
-#define HT_KEY_BACKSPACE        14
-#define HT_KEY_LEFT_BRACKET     26
-#define HT_KEY_RIGHT_BRACKET    27
-#define HT_KEY_ENTER            28
-#define HT_KEY_LEFT_CTRL        29
-#define HT_KEY_SEMICOLON        39
-#define HT_KEY_APOSTROPHE       40
-#define HT_KEY_GRAVE_ACCENT     41
-#define HT_KEY_LEFT_SHIFT       42
-#define HT_KEY_BACKSLASH        43
+#define HT_KEY_Z                XK_z
+#define HT_KEY_X                XK_x
+#define HT_KEY_C                XK_c
+#define HT_KEY_V                XK_v
+#define HT_KEY_B                XK_b
+#define HT_KEY_N                XK_n
+#define HT_KEY_M                XK_m
 
-#define HT_KEY_COMMA            51
-#define HT_KEY_PERIOD           52
-#define HT_KEY_SLASH            53
-#define HT_KEY_RIGHT_SHIFT      54
-#define HT_KEY_KP_MULTIPLY      55
-#define HT_KEY_LEFT_ALT         56
-#define HT_KEY_SPACE            57
-#define HT_KEY_CAPS_LOCK        58
+#define HT_KEY_TAB              XK_Tab
+#define HT_KEY_MINUS            XK_minus
+#define HT_KEY_EQUAL            XK_equal
+#define HT_KEY_BACKSPACE        XK_backspace
+#define HT_KEY_LEFT_BRACKET     XK_parenleft
+#define HT_KEY_RIGHT_BRACKET    XK_parenright
+#define HT_KEY_ENTER            XK_Return
+#define HT_KEY_SEMICOLON        XK_semicolon
+#define HT_KEY_APOSTROPHE       XK_apostrophe
+#define HT_KEY_GRAVE_ACCENT     XK_grave
+#define HT_KEY_LEFT_SHIFT       XK_Shift_L
+#define HT_KEY_RIGHT_SHIFT      XK_Shift_R
+#define HT_KEY_BACKSLASH        XK_backslash
 
-#define HT_KEY_NUM_LOCK         69
-#define HT_KEY_SCROLL_LOCK      70
-#define HT_KEY_KP_7             71
-#define HT_KEY_KP_8             72
-#define HT_KEY_KP_9             73
-#define HT_KEY_KP_SUBTRACT      74
-#define HT_KEY_KP_4             75
-#define HT_KEY_KP_5             76
-#define HT_KEY_KP_6             77
-#define HT_KEY_KP_ADD           78
-#define HT_KEY_KP_1             79
-#define HT_KEY_KP_2             80
-#define HT_KEY_KP_3             81
-#define HT_KEY_KP_0             82
-#define HT_KEY_KP_DECIMAL       83
+#define HT_KEY_COMMA            XK_comma
+#define HT_KEY_PERIOD           XK_period
+#define HT_KEY_SLASH            XK_slash
+#define HT_KEY_KP_MULTIPLY      XK_KP_Multiply
+#define HT_KEY_SPACE            XK_space
+#define HT_KEY_CAPS_LOCK        XK_Caps_Lock
 
-#define HT_KEY_KP_DIVIDE        98
-#define HT_KEY_KP_ENTER         96
+#define HT_KEY_NUM_LOCK         XK_Num_Lock
+#define HT_KEY_SCROLL_LOCK      XK_Scroll_Lock
 
-#define HT_KEY_F1               59
-#define HT_KEY_F2               60
-#define HT_KEY_F3               61
-#define HT_KEY_F4               62
-#define HT_KEY_F5               63
-#define HT_KEY_F6               64
-#define HT_KEY_F7               65
-#define HT_KEY_F8               66
-#define HT_KEY_F9               67
-#define HT_KEY_F10              68
-#define HT_KEY_F11              87
-#define HT_KEY_F12              88
-#define HT_KEY_F13              183
-#define HT_KEY_F14              184
-#define HT_KEY_F15              185
-#define HT_KEY_F16              186
-#define HT_KEY_F17              187
-#define HT_KEY_F18              188
-#define HT_KEY_F19              189
-#define HT_KEY_F20              190
-#define HT_KEY_F21              191
-#define HT_KEY_F22              192
-#define HT_KEY_F23              193
-#define HT_KEY_F24              194
+#define HT_KEY_KP_SUBTRACT      XK_KP_Subtract
+#define HT_KEY_KP_ADD           XK_KP_Add
+#define HT_KEY_KP_DECIMAL       XK_KP_Decimal
+#define HT_KEY_KP_DIVIDE        XK_KP_Divide
+#define HT_KEY_KP_ENTER         XK_KP_Enter
+#define HT_KEY_KP_EQUAL         XK_KP_Equal
+
+#define HT_KEY_KP_0             XK_KP_0
+#define HT_KEY_KP_1             XK_KP_1
+#define HT_KEY_KP_2             XK_KP_2
+#define HT_KEY_KP_3             XK_KP_3
+#define HT_KEY_KP_4             XK_KP_4
+#define HT_KEY_KP_5             XK_KP_5
+#define HT_KEY_KP_6             XK_KP_6
+#define HT_KEY_KP_7             XK_KP_7
+#define HT_KEY_KP_8             XK_KP_8
+#define HT_KEY_KP_9             XK_KP_9
+
+#define HT_KEY_F1               XK_F1 
+#define HT_KEY_F2               XK_F2 
+#define HT_KEY_F3               XK_F3 
+#define HT_KEY_F4               XK_F4 
+#define HT_KEY_F5               XK_F5 
+#define HT_KEY_F6               XK_F6 
+#define HT_KEY_F7               XK_F7 
+#define HT_KEY_F8               XK_F8 
+#define HT_KEY_F9               XK_F9 
+#define HT_KEY_F10              XK_F10
+#define HT_KEY_F11              XK_F11
+#define HT_KEY_F12              XK_F12
+#define HT_KEY_F13              XK_F13
+#define HT_KEY_F14              XK_F14
+#define HT_KEY_F15              XK_F15
+#define HT_KEY_F16              XK_F16
+#define HT_KEY_F17              XK_F17
+#define HT_KEY_F18              XK_F18
+#define HT_KEY_F19              XK_F19
+#define HT_KEY_F20              XK_F20
+#define HT_KEY_F21              XK_F21
+#define HT_KEY_F22              XK_F22
+#define HT_KEY_F23              XK_F23
+#define HT_KEY_F24              XK_F24
 
 
-#define HT_KEY_RIGHT_CTRL       97
-#define HT_KEY_RIGHT_ALT        100
-#define HT_KEY_LINEFEED         101
-#define HT_KEY_HOME             102
-#define HT_KEY_UP               103
-#define HT_KEY_PAGE_UP          104
-#define HT_KEY_LEFT             105
-#define HT_KEY_RIGHT            106
-#define HT_KEY_END              107
-#define HT_KEY_DOWN             108
-#define HT_KEY_PAGE_DOWN        109
-#define HT_KEY_INSERT           110
-#define HT_KEY_DELETE           111
-#define HT_KEY_MACRO            112
-#define HT_KEY_MUTE             113
-#define HT_KEY_VOLUMEDOWN       114
-#define HT_KEY_VOLUMEUP         115
-#define HT_KEY_POWER            116	/* SC System Power Down */
-#define HT_KEY_KP_EQUAL         117
-#define HT_KEY_KP_PLUSMINUS     118
-#define HT_KEY_PAUSE            119
-#define HT_KEY_SCALE            120
+#define HT_KEY_LEFT_CTRL        XK_Control_L
+#define HT_KEY_LEFT_ALT         XK_Alt_L
+#define HT_KEY_RIGHT_CTRL       XK_Control_R
+#define HT_KEY_RIGHT_ALT        XK_Alt_R
+
+#define HT_KEY_LINEFEED         XK_Linefeed
+#define HT_KEY_HOME             XK_Home
+#define HT_KEY_LEFT             XK_Left
+#define HT_KEY_UP               XK_Up
+#define HT_KEY_RIGHT            XK_Right
+#define HT_KEY_DOWN             XK_Down
+
+
+#define HT_KEY_PAGE_UP          XK_Page_Up
+#define HT_KEY_END              XK_End
+#define HT_KEY_PAGE_DOWN        XK_Page_Down
+#define HT_KEY_INSERT           XK_Insert
+#define HT_KEY_DELETE           XK_Delete
+
+#define HT_KEY_PAUSE            XK_Pause
 
 #endif // HT_WINDOWS
