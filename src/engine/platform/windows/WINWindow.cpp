1a0ee8a5f947441cda57fbae4ccd19b84b7e59cd
rares <colorlord98@gmail.com>
2021-02-05 16:09:18 +0200

    Moved event handling function to the bottom

--- src/engine/platform/windows/WINWindow.cpp
+++ src/engine/platform/windows/WINWindow.cpp
@@ -29,6 +29,87 @@ namespace ht { namespace core {
 
 	Window* Window::s_Window = nullptr;
 
+	Window::Window(std::string title, u32 width, u32 height)
+		:m_Title(title), m_Width(width), m_Height(height) {
+
+#if defined(HT_OPENGL)
+		m_Title.append(" - OpenGL");
+#elif defined (HT_DIRECTX)
+		m_Title.append(" - DirectX");
+#endif // HT_OPENGL
+
+		WNDCLASSEXA wcex = {};
+
+		wcex.cbSize         = sizeof(WNDCLASSEX);
+		wcex.lpfnWndProc    = WndProc;
+		wcex.cbClsExtra     = 0;
+		wcex.cbWndExtra     = 0;
+		wcex.hInstance      = GetModuleHandleA(nullptr);
+		wcex.hIcon          = nullptr;
+		wcex.hCursor        = LoadCursorA(NULL, MAKEINTRESOURCEA(32512));
+		wcex.hbrBackground  = 0;
+		wcex.lpszClassName  = "Helltooth window";
+		wcex.lpszMenuName   = 0;
+		wcex.hIconSm        = 0;
+		wcex.style          = CS_VREDRAW | CS_HREDRAW | CS_OWNDC;
+
+		if (!RegisterClassExA(&wcex)) {
+			DWORD word = GetLastError();
+			HT_FATAL("[Window] Could not register window class, %d!", word);
+			return;
+		}
+
+		m_Hwnd = CreateWindowA("Helltooth window", m_Title.c_str(), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, m_Width, m_Height, 0, 0, wcex.hInstance, 0);
+
+		if (!m_Hwnd) {
+			DWORD word = GetLastError();
+			HT_FATAL("[Window] Could not create window, %d!", word);
+			return;
+
+		}
+
+		SetVisible(true);
+		SetFocus(m_Hwnd);
+		UpdateWindow(m_Hwnd);
+		HT_ASSERT(s_Window == nullptr, "Multiple windows not supported!");
+		s_Window = this;
+		m_Context = new Context(m_Hwnd);
+	}
+
+	Window::~Window() {
+		DestroyWindow(m_Hwnd);
+		m_Hwnd = nullptr; 
+		delete m_Context;
+		s_Window = nullptr;
+	}
+
+	void Window::Update() {
+		MSG msg;
+		if (PeekMessageA(&msg, m_Hwnd, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessageA(&msg);
+		}
+		m_Context->Update();
+	}
+
+	void Window::Clear() {
+		m_Context->Clear();
+	}
+
+	void Window::SetVisible(bool visible) {
+		ShowWindow(m_Hwnd, visible ? SW_SHOW : SW_HIDE);
+		m_IsVisible = visible;
+	}
+
+	void Window::SetTitle(std::string title) {
+#if defined(HT_OPENGL)
+		title.append(" - OpenGL");
+#elif defined(HT_DIRECTX)
+		title.append(" - DirectX");
+#endif // HT_OPENGL
+		SetWindowTextA(m_Hwnd, title.c_str());
+	}
+
 	LRESULT Window::WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 		Window* window = Window::GetWindow();
 
@@ -117,87 +198,6 @@ namespace ht { namespace core {
 			EventDispatcher::Dispatch(e);
 		delete e;
 		return DefWindowProc(hwnd, msg, wParam, lParam);
-	}
-
-	Window::Window(std::string title, u32 width, u32 height)
-		:m_Title(title), m_Width(width), m_Height(height) {
-
-#if defined(HT_OPENGL)
-		m_Title.append(" - OpenGL");
-#elif defined (HT_DIRECTX)
-		m_Title.append(" - DirectX");
-#endif // HT_OPENGL
-
-		WNDCLASSEXA wcex = {};
-
-		wcex.cbSize         = sizeof(WNDCLASSEX);
-		wcex.lpfnWndProc    = WndProc;
-		wcex.cbClsExtra     = 0;
-		wcex.cbWndExtra     = 0;
-		wcex.hInstance      = GetModuleHandleA(nullptr);
-		wcex.hIcon          = nullptr;
-		wcex.hCursor        = LoadCursorA(NULL, MAKEINTRESOURCEA(32512));
-		wcex.hbrBackground  = 0;
-		wcex.lpszClassName  = "Helltooth window";
-		wcex.lpszMenuName   = 0;
-		wcex.hIconSm        = 0;
-		wcex.style          = CS_VREDRAW | CS_HREDRAW | CS_OWNDC;
-
-		if (!RegisterClassExA(&wcex)) {
-			DWORD word = GetLastError();
-			HT_FATAL("[Window] Could not register window class, %d!", word);
-			return;
-		}
-
-		m_Hwnd = CreateWindowA("Helltooth window", m_Title.c_str(), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, m_Width, m_Height, 0, 0, wcex.hInstance, 0);
-
-		if (!m_Hwnd) {
-			DWORD word = GetLastError();
-			HT_FATAL("[Window] Could not create window, %d!", word);
-			return;
-
-		}
-
-		SetVisible(true);
-		SetFocus(m_Hwnd);
-		UpdateWindow(m_Hwnd);
-		HT_ASSERT(s_Window == nullptr, "Multiple windows not supported!");
-		s_Window = this;
-		m_Context = new Context(m_Hwnd);
-	}
-
-	Window::~Window() {
-		DestroyWindow(m_Hwnd);
-		m_Hwnd = nullptr; 
-		delete m_Context;
-		s_Window = nullptr;
-	}
-
-	void Window::Update() {
-		MSG msg;
-		if (PeekMessageA(&msg, m_Hwnd, 0, 0, PM_REMOVE)) {
-			TranslateMessage(&msg);
-			DispatchMessageA(&msg);
-		}
-		m_Context->Update();
-	}
-
-	void Window::Clear() {
-		m_Context->Clear();
-	}
-
-	void Window::SetVisible(bool visible) {
-		ShowWindow(m_Hwnd, visible ? SW_SHOW : SW_HIDE);
-		m_IsVisible = visible;
-	}
-
-	void Window::SetTitle(std::string title) {
-#if defined(HT_OPENGL)
-		title.append(" - OpenGL");
-#elif defined (HT_DIRECTX)
-		title.append(" - DirectX");
-#endif // HT_OPENGL
-		SetWindowTextA(m_Hwnd, title.c_str());
 	}
 
 } }
