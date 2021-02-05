36cd87dfbe96cd9c9e278184df0c3ac47144cfeb
rares <colorlord98@gmail.com>
2021-02-05 14:26:57 +0200

    Changed buildandrun script to use the build script

--- buildandrun.sh
+++ buildandrun.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-
-res_location=bin/Debug-GL/10000Cubes/res/
-
-if [ ! -d $res_location ]; then
-	cp -R src/demo/res $res_location
-fi
-
-make -d config=debug-gl_x64 -C Solution/
-
+./build.sh
 cd bin/Debug-GL/10000Cubes/
 ./10000Cubes
